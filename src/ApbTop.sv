2da8b813ffc07734872b771903395a9daad42ad9
Nisha Shetty <109810083+nishashetty11@users.noreply.github.com>
2025-04-24 11:05:35 +0530

    Create ApbTop.sv

---
+++ src/ApbTop.sv
@@ -0,0 +1,21 @@
+`include "ApbPackage.sv"
+
+module top;
+ bit pclk,presetn;
+ always #5 pclk=~pclk;
+ initial begin
+     pclk=0;
+     presetn =0;
+     #20;
+     presetn=1;
+    end
+ ApbIntf intf(pclk,presetn);
+initial begin
+       uvm_config_db #(virtual ApbInterface)::set(uvm_root::get(),"*","vif",intf);
+     end
+/*
+initial begin
+  run_test("apb_regression_test");
+
+end*/
+endmodule
