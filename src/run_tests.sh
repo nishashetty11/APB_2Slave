5d2b7a28d42558a04456505ef52b19d40c5b68c7
nishashetty <109810083+nishashetty11@users.noreply.github.com>
2025-05-29 12:33:19 +0530

    updated makefile

--- src/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p logs
-
-while read testname; do
-  echo "Running test: $testname"
-  vsim -c work.top +UVM_TESTNAME=$testname -do "run -all; exit" -l logs/$testname.log
-done < testlist.txt
-
